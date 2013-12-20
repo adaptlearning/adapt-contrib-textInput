1b0db5466a20f183fcf4f48a020caeed5402a801
Kev Adsett <kev.adsett@gmail.com>
2013-12-20 10:44:17 +0000

    Copied in methods from previous version

---
+++ js/adapt-contrib-textInput.js
@@ -0,0 +1,90 @@
+/*
+* adapt-contrib-textInput
+* License - http://github.com/adaptlearning/adapt_framework/LICENSE
+* Maintainers - Kev Adsett <kev.adsett@gmail.com>
+*/
+
+define(function (require) {
+    var QuestionView = require('coreViews/questionView');
+    var Adapt = require('coreJS/adapt');
+    
+    var TextInput = QuestionView.extend({
+        canSubmit:function() {
+            this.$(".textbox").each(function() {
+                if($(this).val()=="") {
+                    return false;  
+                }
+            });
+            return true;
+        },
+        
+        checkAnswerIsCorrect: function(possibleAnswers, userAnswer) {
+            var answerIsCorrect = _.contains(possibleAnswers, this.cleanupUserAnswer(userAnswer));
+            if(answerIsCorrect) this.model.set('atLeastOneCorrectSelection', true);
+            return answerIsCorrect;
+        },
+        
+        cleanupUserAnswer: function(userAnswer) {
+            if(this.model.get('allowAnyCase')) {
+                userAnswer = userAnswer.toLowerCase();
+            }
+            if(this.model.get('allowPunctuation')) {
+                var userAnswerClean = userAnswer.replace(/[\.,-\/#!$£%\^&\*;:{}=\-_`~()]/g,"");
+                userAnswer = $.trim(userAnswerClean);
+            }
+            return userAnswer;
+        },
+        
+        forEachAnswer: function(callback) {
+             _.each(this.model.get('items'), function(item, index) {
+                if(this.model.get('allowAnyOrder')) {
+                    this.$(".textbox").each($.proxy(function(index, element) {
+                        var userAnswer = $(element).val();
+                        callback(this.checkAnswerIsCorrect(item.answers, userAnswer), item);
+                    },this));
+                } else {
+                    var userAnswer = this.$(".textbox").eq(index).val();
+                    callback(this.checkAnswerIsCorrect(item.answers, userAnswer), item);
+                }
+            }, this);
+        },
+        
+        markQuestion: function() {
+            this.forEachAnswer(function(correct, item) {
+                item.correct = correct;
+            });
+            QuestionView.prototype.markQuestion.apply(this);
+        },
+        
+        onEnabledChanged: function() {
+            this.$('.textbox').prop('disabled', !this.model.get('enabled'));
+        },
+        
+        onModelAnswerShown:function () {
+            _.each(this.model.get('items'), function(item, index){
+                this.$(".textbox").eq(index).val(item.answers[0]);
+            }, this);
+        },
+        
+        onUserAnswerShown:function () {
+            _.each(this.model.get('items'), function(item, index){
+                this.$(".textbox").eq(index).val(item.userAnswer);
+            }, this);
+        },
+        
+        postRender: function() {
+            QuestionView.prototype.postRender.apply(this);
+            this.setReadyStatus();
+        },
+        
+        storeUserAnswer: function() {
+            _.each(this.model.get('items'), function(item, index) {
+                item.userAnswer = this.$('.textbox').eq(index).val();
+            }, this);
+        }
+        
+    });
+    
+    Adapt.register("textInput", textInput);
+    
+});
