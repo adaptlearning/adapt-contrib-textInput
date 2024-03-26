a8540084d1037799cf5f6a42a414f70e92e84a6d
Lemmy Adams <lemmy.adams@learningpool.com>
2024-03-26 11:16:46 +0000

    Added text input test

---
+++ test/e2e/textInput.cy.js
@@ -0,0 +1,27 @@
+describe('Text Input', function () {
+  beforeEach(function () {
+    cy.getData()
+    cy.visit('/');
+  });
+
+  it('should display the text input component', function () {
+    const textInputComponents = this.data.components.filter((component) => component._component === 'textinput')
+    textInputComponents.forEach((textInputComponent) => {
+      cy.visit(`/#/preview/${textInputComponent._id}`);
+      const bodyWithoutHtml = textInputComponent.body.replace(/<[^>]*>/g, '');
+      
+      cy.testQuestionButtons()
+      cy.testContainsOrNotExists('.textinput__title', textInputComponent.displayTitle)
+      cy.testContainsOrNotExists('.textinput__body', bodyWithoutHtml)
+      cy.testContainsOrNotExists('.textinput__instruction', textInputComponent.instruction)
+      cy.get('.textinput-item__textbox').should('have.length', 1)
+      if (textInputComponent._items[0].placeholder) {
+        cy.get('.textinput-item__textbox').should('have.attr', 'placeholder', textInputComponent._items[0].placeholder)
+      }
+      
+      // Make sure the current component is tested before moving to the next one
+      // Custom cypress tests are async so we need to wait for them to pass first
+      cy.wait(1000)
+    });
+  });
+});
