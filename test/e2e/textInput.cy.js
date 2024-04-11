e767e02dc069a1eab6b56391a3aa57479dc38ca2
Lemmy Adams <lemmy.adams@learningpool.com>
2024-04-11 14:32:10 +0100

    PR feedback

--- test/e2e/textInput.cy.js
+++ test/e2e/textInput.cy.js
@@ -1,26 +1,26 @@
 describe('Text Input', function () {
   beforeEach(function () {
-    cy.getData()
+    cy.getData();
     cy.visit('/');
   });
 
   it('should display the text input component', function () {
-    const textInputComponents = this.data.components.filter((component) => component._component === 'textinput')
-    textInputComponents.forEach((textInputComponent) => {
+    const textInputComponents = this.data.components.filter(component => component._component === 'textinput');
+    textInputComponents.forEach(textInputComponent => {
       cy.visit(`/#/preview/${textInputComponent._id}`);
-      const bodyWithoutHtml = cy.helpers.stripHtml(textInputComponent.body)
-      cy.testContainsOrNotExists('.textinput__body', bodyWithoutHtml)
+      const stripHtml = cy.helpers.stripHtml;
+      cy.testContainsOrNotExists('.textinput__body', stripHtml(textInputComponent.body));
+      cy.testContainsOrNotExists('.textinput__title', stripHtml(textInputComponent.displayTitle));
+      cy.testContainsOrNotExists('.textinput__instruction', stripHtml(textInputComponent.instruction));
 
-      cy.testContainsOrNotExists('.textinput__title', textInputComponent.displayTitle)
-      cy.testContainsOrNotExists('.textinput__instruction', textInputComponent.instruction)
-      cy.get('.textinput-item__textbox').should('have.length', 1)
+      cy.get('.textinput-item__textbox').should('have.length', 1);
       if (textInputComponent._items[0].placeholder) {
-        cy.get('.textinput-item__textbox').should('have.attr', 'placeholder', textInputComponent._items[0].placeholder)
-      }
-      
+        cy.get('.textinput-item__textbox').should('have.attr', 'placeholder', textInputComponent._items[0].placeholder);
+      };
+
       // Make sure the current component is tested before moving to the next one
       // Custom cypress tests are async so we need to wait for them to pass first
-      cy.wait(1000)
+      cy.wait(1000);
     });
   });
 });
