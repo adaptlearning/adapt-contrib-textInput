ffa909da16a652bc775a6b70a73c79c850a990ff
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2024-07-10 11:23:11 +0100

    suffix moved to aria-describedby

--- templates/textinput.jsx
+++ templates/textinput.jsx
@@ -61,7 +61,8 @@ export default function TextInput (props) {
                 placeholder={placeholder}
                 data-id={`${input}-${index}`}
                 id={`${_id}-${index}`}
-                aria-labelledby={(prefix || suffix) && `${_id}-${index}-aria-prefix ${_id}-${index}-aria-suffix`}
+                aria-labelledby={prefix && `${_id}-${index}-aria-prefix`}
+                aria-describedby={suffix && `${_id}-${index}-aria-suffix`}
                 aria-label={placeholder}
                 defaultValue={userAnswer}
                 disabled={!_isEnabled}
